ad7adc1134b7ea211589ae294d1dcda7ae9fe20e
Arun kumar <ak2965491@gmail.com>
2023-05-19 09:24:01 +0530

    submitting code-Fri May 19 2023 09:23:59 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const App = () => {
+  return ReactDOM.render(
+    <p>
+      Now I can render any React component on any DOM node I want using
+      ReactDOM.render
+    </p>,
+    document.getElementById("root")
+  );
+};
 
-
-    return (
-      <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-    )
-  }
-  
-  
-  export default App;
+export default App;

58c44fba10479921686101b6d37a95955d27ed45
Arun kumar <ak2965491@gmail.com>
2023-05-19 09:26:17 +0530

    submitting code-Fri May 19 2023 09:26:15 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
+import React, {Component, useState} from "react";
+import '../styles/App.css';
 const App = () => {
-  return ReactDOM.render(
-    <p>
-      Now I can render any React component on any DOM node I want using
-      ReactDOM.render
-    </p>,
-    document.getElementById("root")
-  );
-};
+  return (
+    <div id="main">
+      <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    </div>
+  )
+}
+
 
 export default App;
